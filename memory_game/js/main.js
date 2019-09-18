483e4944a0c1d8b17620032a0f387b96bad2fb8c
Veronica Ramirez <veronicaramirez@Veronicas-MBP.frontiernet.net>
2019-09-17 20:19:39 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,26 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank:"Queen",
+	suit:"Hearts",
+	cardImage:"images/queen-of-hearts.png"
+,
+},
+{
+	rank:"Queen",
+	suit:"Diamonds",
+	cardImage:"images/queen-of-diamonds.png",
+},
+{
+	rank:"King",
+	suit:"Hearts",
+	cardImage:"images/king-of-hearts.png",
+},
+{
+	rank:"King",
+	suit:"Diamonds",
+	cardImage:"images/king-of-diamonds.png",
+},
+];
 const cardsInPlay = [];
 function checkForMatch() {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -8,11 +30,13 @@ function checkForMatch() {
 		}
 }
 function flipCard(cardId) {
-	console.log("User flipped" + " " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped" + " " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	if (cardsInPlay.length === 2) {
 	checkForMatch();
 	}
 }
 flipCard(0);
-flipCard(1);
+flipCard(2);
